d3b88b9c3132beaba7693720217ea567e9c32f80
ebina4yaka <4yaka3bina@gmail.com>
2020-10-29 16:52:27 +0900

    有給取得の時季指定書の追加

--- src/libs/export/exportDocx.ts
+++ src/libs/export/exportDocx.ts
@@ -66,12 +66,88 @@ function createResignationReport(parameters: ExportParameters): Paragraph[] {
   return top.concat(contents).concat(bottom)
 }
 
+// 有給取得の時季指定書
+function createPaidLeaveRequestForm(parameters: ExportParameters): Paragraph[] {
+  const top = [
+    new Paragraph({
+      text: '有給取得の時季指定書',
+      alignment: AlignmentType.CENTER,
+    }),
+    new Paragraph({
+      text: `${parameters.companyName} 御中`,
+      alignment: AlignmentType.LEFT,
+    }),
+    new Paragraph({
+      text: `（代表取締役 ${parameters.representativeDirector} 様）`,
+      alignment: AlignmentType.LEFT,
+    }),
+  ]
+
+  const applicant = (() => {
+    if (parameters.department !== '') {
+      return [
+        new Paragraph({}),
+        new Paragraph({
+          text: `申請日 ${parameters.dateOfNotification}`,
+          alignment: AlignmentType.LEFT,
+        }),
+        new Paragraph({
+          text: `所属部署 ${parameters.department}`,
+          alignment: AlignmentType.LEFT,
+        }),
+        new Paragraph({
+          text: `氏名 ${parameters.name}`,
+          alignment: AlignmentType.LEFT,
+        }),
+        new Paragraph({}),
+      ]
+    }
+    return [
+      new Paragraph({}),
+      new Paragraph({
+        text: `申請日 ${parameters.dateOfNotification}`,
+        alignment: AlignmentType.LEFT,
+      }),
+      new Paragraph({
+        text: `氏名 ${parameters.name}`,
+        alignment: AlignmentType.LEFT,
+      }),
+      new Paragraph({}),
+    ]
+  })()
+
+  const contents = [
+    new Paragraph({}),
+    new Paragraph({
+      text:
+        '労働基準法39条5項に基づき、下記日時を有給使用の時季として指定させていただきますので、よろしくお願いします。',
+      alignment: AlignmentType.LEFT,
+    }),
+    new Paragraph({}),
+  ]
+
+  const date = [
+    new Paragraph({
+      text: `${parameters.dateOfNotification}～${parameters.dateOfRetirement}`,
+      alignment: AlignmentType.LEFT,
+    }),
+  ]
+
+  return top.concat(applicant).concat(contents).concat(date)
+}
+
 export default function exportDocx(parameters: ExportParameters): void {
   const doc = new Document()
 
   doc.addSection({
     children: createResignationReport(parameters),
   })
+
+  if (parameters.daysOfPaidLeaveRemaining >= 1) {
+    doc.addSection({
+      children: createPaidLeaveRequestForm(parameters),
+    })
+  }
 
   Packer.toBlob(doc).then((blob) => {
     saveAs(blob, `${parameters.companyName}_退職届.docx`)
