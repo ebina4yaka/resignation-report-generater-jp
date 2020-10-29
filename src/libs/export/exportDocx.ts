bceffb76a0eca3defeca27b1e007617c83c91a4f
ebina4yaka <4yaka3bina@gmail.com>
2020-10-29 16:23:30 +0900

    fix docx contents

--- src/libs/export/exportDocx.ts
+++ src/libs/export/exportDocx.ts
@@ -1,42 +1,76 @@
-import { Document, Packer, Paragraph, TextRun } from 'docx'
+import { AlignmentType, Document, Packer, Paragraph } from 'docx'
 import { saveAs } from 'file-saver'
 import { ExportParameters } from './ExportParameters'
 
 export default function exportDocx(parameters: ExportParameters): void {
-  // Create document
   const doc = new Document()
 
-  // Documents contain sections, you can have multiple
-  // sections per document, go here to learn more about sections
-  // This simple example will only contain one section
+  const top = [
+    new Paragraph({
+      text: '退職届',
+      alignment: AlignmentType.CENTER,
+    }),
+    new Paragraph({
+      text: `${parameters.companyName} 御中`,
+      alignment: AlignmentType.LEFT,
+    }),
+    new Paragraph({
+      text: `（代表取締役 ${parameters.representativeDirector} 様）`,
+      alignment: AlignmentType.LEFT,
+    }),
+  ]
+
+  const contents = [
+    new Paragraph({}),
+    new Paragraph({
+      text:
+        `このたび${parameters.reason}により${parameters.dateOfRetirement}をもって` +
+        `退職することといたしましたので、民法627条1項に基づき、本日、労働契約について解約の申し入れをいたします。`,
+      alignment: AlignmentType.LEFT,
+    }),
+    new Paragraph({
+      text:
+        '※なお、離職票は退職日経過後に私の住所に郵送いただきますようお願い申し上げます。',
+      alignment: AlignmentType.LEFT,
+    }),
+    new Paragraph({}),
+  ]
+
+  const bottom = (() => {
+    if (parameters.department !== '') {
+      return [
+        new Paragraph({
+          text: parameters.dateOfNotification,
+          alignment: AlignmentType.LEFT,
+        }),
+        new Paragraph({
+          text: parameters.department,
+          alignment: AlignmentType.LEFT,
+        }),
+        new Paragraph({
+          text: parameters.name,
+          alignment: AlignmentType.LEFT,
+        }),
+      ]
+    }
+    return [
+      new Paragraph({
+        text: parameters.dateOfNotification,
+        alignment: AlignmentType.LEFT,
+      }),
+      new Paragraph({
+        text: parameters.name,
+        alignment: AlignmentType.LEFT,
+      }),
+    ]
+  })()
+
+  const section = (() => {
+    return top.concat(contents).concat(bottom)
+  })()
+
   doc.addSection({
-    properties: {},
-    children: [
-      new Paragraph({
-        children: [new TextRun(parameters.name)],
-      }),
-      new Paragraph({
-        children: [new TextRun(parameters.companyName)],
-      }),
-      new Paragraph({
-        children: [new TextRun(parameters.department)],
-      }),
-      new Paragraph({
-        children: [new TextRun(parameters.representativeDirector)],
-      }),
-      new Paragraph({
-        children: [new TextRun(parameters.dateOfNotification)],
-      }),
-      new Paragraph({
-        children: [new TextRun(parameters.dateOfRetirement)],
-      }),
-      new Paragraph({
-        children: [new TextRun(parameters.reason)],
-      }),
-      new Paragraph({
-        children: [new TextRun(String(parameters.daysOfPaidLeaveRemaining))],
-      }),
-    ],
+    children: section,
   })
 
   Packer.toBlob(doc).then((blob) => {
