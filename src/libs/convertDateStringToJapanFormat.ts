40888e015d6b7f801a14f31ae875baf8c0f12685
ebina4yaka <4yaka3bina@gmail.com>
2020-10-30 08:51:54 +0900

    change japan date format

--- src/libs/convertDateStringToJapanFormat.ts
+++ src/libs/convertDateStringToJapanFormat.ts
@@ -5,5 +5,5 @@ export default function convertDateStringToJapanFormat(
   dateString: string
 ): string {
   dayjs.locale('ja')
-  return dayjs(dateString).format('YYYY年MM月DD日 dddd')
+  return dayjs(dateString).format('YYYY年MM月DD日')
 }
